5e7f9d50561c3acc2663f075aac4ee3ce308641f
MinnAK686 <anastasioak327@gmail.com>
2023-03-08 22:26:56 +0630

    APshopping template setup - Clients side

--- controllers/PagesController.php
+++ controllers/PagesController.php
@@ -6,6 +6,23 @@ class PagesController
     {
         view("home");
     }
+    public static function productDetail()
+    {
+        view("product-detail");
+    }
+    public static function addToCart()
+    {
+        view("add-to-cart");
+    }
+    public static function productsCheckout()
+    {
+        view("checkout");
+    }
+    public static function categories()
+    {
+        view("categories");
+    }
+
     public static function about()
     {
         view("about");
--- core/Router/Routes.php
+++ core/Router/Routes.php
@@ -6,5 +6,16 @@ Route::get("", [PagesController::class, "home"]);
 Route::get("about", [PagesController::class, "about"]);
 Route::get("contact", [PagesController::class, "contact"]);
 
+Route::get("products/product-detail", [PagesController::class, "productDetail"]);
+Route::get("products/checkout", [PagesController::class, "productsCheckout"]);
+Route::get("cart", [PagesController::class, "addToCart"]);
+Route::get("categories", [PagesController::class, "categories"]);
+
+// Clients Side Authentication
+Route::get("login", [AuthController::class, "login"]);
+Route::get("register", [AuthController::class, "register"]);
+Route::post("login", [AuthController::class, "login"]);
+Route::post("register", [AuthController::class, "register"]);
+
 Route::get("admin", [AdminPagesController::class, "dashboard"]);
 Route::get("admin/tables", [AdminPagesController::class, "tables"]);
