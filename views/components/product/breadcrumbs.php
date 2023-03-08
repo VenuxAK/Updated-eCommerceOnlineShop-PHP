ef03d9b81e2a061b228b0d9bb95ecdca9acad740
MinnAK686 <anastasioak327@gmail.com>
2023-03-08 20:20:22 +0630

    ShopGrids template setup - clients side

---
+++ views/components/product/breadcrumbs.php
@@ -0,0 +1,20 @@
+<!-- Start Breadcrumbs -->
+<div class="breadcrumbs">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-6 col-md-6 col-12">
+                <div class="breadcrumbs-content">
+                    <h1 class="page-title">Single Product</h1>
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-12">
+                <ul class="breadcrumb-nav">
+                    <li><a href="/"><i class="lni lni-home"></i> Home</a></li>
+                    <li><a href="/">Shop</a></li>
+                    <li>Single Product</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End Breadcrumbs -->
--- views/partials/Header.php
+++ views/partials/Header.php
@@ -1,8 +1,41 @@
-<html lang="en">
+<!DOCTYPE html>
+<html class="no-js" lang="zxx">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>eCommerceOS</title>
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <title>ShopGrids - Bootstrap 5 eCommerce HTML Template.</title>
+    <meta name="description" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <base href="<?= BASE_URL ?>">
+    <link rel="shortcut icon" type="image/x-icon" href="views/assets/images/favicon.svg" />
+
+    <!-- ========================= CSS here ========================= -->
+    <link rel="stylesheet" href="views/assets/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="views/assets/css/LineIcons.3.0.css" />
+    <link rel="stylesheet" href="views/assets/css/tiny-slider.css" />
+    <link rel="stylesheet" href="views/assets/css/glightbox.min.css" />
+    <link rel="stylesheet" href="views/assets/css/main.css" />
+    <link rel="stylesheet" href="views/assets/css/app.css" />
+
 </head>
+
 <body>
+    <!--[if lte IE 9]>
+      <p class="browserupgrade">
+        You are using an <strong>outdated</strong> browser. Please
+        <a href="https://browsehappy.com/">upgrade your browser</a> to improve
+        your experience and security.
+      </p>
+    <![endif]-->
+
+    <!-- Preloader -->
+    <div class="preloader">
+        <div class="preloader-inner">
+            <div class="preloader-icon">
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+    </div>
+    <!-- /End Preloader -->
